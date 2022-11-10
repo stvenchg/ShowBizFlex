8fc108da15d5c70beb566c474fabcf3e5af8b7ad
stvenchg <stvenching@gmail.com>
2022-11-10 21:54:05 +0100

    Affichage sur la page d'accueil  depuis l'API de TMDB
    - Affichage populaire
    - Affichage tendances
    - Affichage mieux notés

--- Layout.php
+++ Layout.php
@@ -8,7 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="../Assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link type="text/css" rel="stylesheet" href="../Assets/css/lightslider.css" />
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script src="../Assets/js/lightslider.js"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
@@ -23,9 +25,31 @@
 
     <main>
         <?php
-            global $view;
-            echo $view;
+        global $view;
+        echo $view;
         ?>
+
+        <!-- Les séries qui viennent de sortir -->
+        <h4 class="latest-heading">LES DERNIÈRES SORTIES</h4>
+
+        <ul id="autoWidthLatest" class="cs-hidden">
+            <li class="item-a">
+                <div class="latest-box">
+                    <a href="#"><img src="../Assets/images/demo/l-1.jpg"></a>
+                </div>
+            </li>
+            <li class="item-b">
+                <div class="latest-box">
+                    <img src="../Assets/images/demo/l-2.jpg">
+                </div>
+            </li>
+            <li class="item-c">
+                <div class="latestv-box">
+                    <img src="../Assets/images/demo/l-3.jpg">
+                </div>
+            </li>
+        </ul>
+        
     </main>
 
     <footer class="footer">
@@ -36,6 +60,7 @@
 
     <script src="https://kit.fontawesome.com/e477e9361e.js" crossorigin="anonymous"></script>
     <script src="../Assets/js/script.js"></script>
+    <script src="../Assets/js/slider.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 
