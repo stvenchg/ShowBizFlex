2a9128301cdcdd22fb62d814a13f13a2b69c6b92
stvenchg <stvenching@gmail.com>
2022-11-20 18:29:47 +0100

    Ajout d'informations complémentaire sur la fiche série
    - Amélioration du menu déroulant de l'utilisateur
    - Ajout du casting
    - Ajout des diffuseurs
    - Ajout du nom d'origine
    - Ajout de la durée moyenne d'un épisode
    - Ajout d'un bouton voir bande annonce
    - Ajout d'un bouton ajouter en favori
    - Le backdrop se situe derrière la barre de navigation sur la fiche série

--- Modules/ModShows/View_Shows.php
+++ Modules/ModShows/View_Shows.php
@@ -20,15 +20,49 @@ class ViewShows extends GenericView
     public function show_overview()
     {
 
+        // Style barre de navigation transparente
+        echo '<style>
+            header {
+                position: absolute;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                z-index: 10000;
+                height: 5%;
+            }
+        </style>';
+
         $details = $this->model->getDetails();
         $showRating = $this->model->getContentRating();
         $showTrailer = $this->model->getVideos();
         $watchProviders = $this->model->getWatchProviders();
+        $externalIds = $this->model->getExternalIds();
+        $showCast = $this->model->getCast();
 
-        foreach($showTrailer['results'] as $index => $value) {
-            if ($index == 0) {
-                $trailer = $value['key'];
+        $showCastPicture = array();
+        foreach($showCast['cast'] as $index => $value) {
+            array_push($showCastPicture, $showCast['cast'][$index]['profile_path']);
+        }
+
+        $showCastPictureString = '';
+        foreach ($showCastPicture as $index => $castPic) {
+            $showCastPictureString .= '<li class="item-' . $index . '">
+            <div class="cast-box">
+                <a href="#"><img src="https://image.tmdb.org/t/p/w200' . $castPic . '"></a>
+            </div>
+        </li>';
+        }
+
+        if (!empty($showTrailer['results'])) {
+            foreach($showTrailer['results'] as $index => $value) {
+                if ($index == 0) {
+                    $trailer = $value['key'];
+                }
             }
+        }
+        else {
+            $trailer = '';
         }
 
         $loopGenres = '';
@@ -57,12 +91,38 @@ class ViewShows extends GenericView
             $providersString = "Information indisponible.";
         }
 
+        if (isset($details['networks'])) {
+            $networks = array();
+            foreach ($details['networks'] as $network) {
+                array_push($networks, $network['logo_path']);
+            }
+            
+            $networksString = '';
+            foreach ($networks as $network) {
+                $networksString .= '<img src="https://image.tmdb.org/t/p/w154' . $network . '"></img><br /><br />';
+            }
+        }
+        else {
+            $networksString = "Information indisponible.";
+        }
+
+        // header
         $fullBackdropPath = "https://image.tmdb.org/t/p/original" . $details['backdrop_path'];
         $fullPosterPath = "https://image.tmdb.org/t/p/w500" . $details['poster_path'];
         $showName = $details['name'];
         $showFirstAirYear = ' (' . strtok($details['first_air_date'], '-') . ')';
-        $showGenres = rtrim($loopGenres, ', ') . ' - ' . '[durée moyenne à récupérer]';
-        $showSynopsis = $details['overview'];
+        $episodeRunTime = $details['episode_run_time'][0];
+        $showGenres = rtrim($loopGenres, ', ') . ' - ' . $episodeRunTime . 'm';
+        $showSynopsis = str_replace('...', '.', $details['overview']);
+        $showSynopsisB = str_replace('.', '.<br />', $showSynopsis);
+        $tagLine = $details['tagline'];
+
+        // body
+        $showWebsite = $details['homepage'];
+        $originalName = $details['original_name'];
+        $status = $details['status'];
+        $type = $details['type'];
+        $originalLanguage = $details['original_language'];
 
         echo '<div class="show-box">';
 
@@ -77,18 +137,67 @@ class ViewShows extends GenericView
                 <img src="' . $fullPosterPath . '"/>
             </div>
             <div class="showMainInfo">
-                <h1 class="show-title">' . $showName . '<span class="show-release-date">' . $showFirstAirYear . '</span>'. '</h1>
+                <h1 class="show-title"><a style="color: white" href="#">' . $showName . '</a><span class="show-release-date">' . $showFirstAirYear . '</span>'. '</h1>
                 <span class="show-rating">' . $rating . '</span><span class="show-genres">' . $showGenres . '</span>
-                <div><a target="_blank" href="https://youtube.com/embed/' . $trailer . '"><button class="showTrailerButton"><i class="fa-solid fa-play"></i>  Bande-annonce</button></a></div>
+                <div class="showMainControls">
+                    <a target="_blank" href="https://youtube.com/embed/' . $trailer . '"><button class="showTrailerButton"><i class="fa-solid fa-play"></i>  Bande-annonce</button></a>
+                    <div class="showSubControls">
+                        <a href="#"><button class="favButton"><i class="fa-solid fa-heart"></i></button></a>
+                    </div>
+                </div>
+                <h2 class="show-tagline">' . $tagLine . '</h2>
                 <h3 class="section-title">Synopsis</h3>
-                <p class="show-synopsis">' . $showSynopsis . '</p>
+                <p class="show-synopsis">' .  $showSynopsisB . '</p>
                 <h3 class="section-title">Où regarder ?</h3>
                 <div class="show-availability">'. $providersString . '</div>
             </div>';
 
         echo '</div>';
-
+        
         echo '</div>';
+
+        // Display more infos
+        echo '<div class="body-show-container">
+        
+            <div class="showFactsLeftColumn">
+                <a target="_blank" href="'. $showWebsite . '"><h4 class="fact-title ta-center"><i class="fa-solid fa-link"></i> Accéder au site officiel</h4></a>
+
+                <h4 class="fact-title">Faits principaux</h4>
+                <div class="fact-wrap">
+                    <h4 class="fact-subtitle">Nom d\'origine</h4>
+                    <h4 class="fact">' . $originalName . '</h4>
+                </div>
+
+                <div class="fact-wrap">
+                    <h4 class="fact-subtitle">Statut</h4>
+                    <h4 class="fact">' . $status . '</h4>
+                </div>
+
+                <div class="fact-wrap">
+                    <h4 class="fact-subtitle">Diffuseurs télévisés</h4>
+                    <h4 class="fact">' . $networksString . '</h4>
+                </div>
+
+                <div class="fact-wrap">
+                    <h4 class="fact-subtitle">Type</h4>
+                    <h4 class="fact">' . $type . '</h4>
+                </div>
+
+                <div class="fact-wrap">
+                    <h4 class="fact-subtitle">Langue d\'origine</h4>
+                    <h4 class="fact">' . $originalLanguage . '</h4>
+                </div>
+            </div>
+
+            <div class="showPanel">
+                <h2 class="panel-title">Distribution des rôles</h2>
+
+                <ul id="autoWidthTopRated" class="cs-hidden">
+                    ' .$showCastPictureString . '
+                </ul>
+            </div>
+
+        </div>';
 
         /*
         
