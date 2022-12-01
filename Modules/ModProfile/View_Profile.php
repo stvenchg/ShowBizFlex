8a101ef2dab575f11b5fed85058b4440cbb63b49
Steven Ching <96243582+stvenchg@users.noreply.github.com>
2022-12-01 15:00:05 +0100

    Amélioration style profil

--- Modules/ModProfile/View_Profile.php
+++ Modules/ModProfile/View_Profile.php
@@ -39,25 +39,77 @@ class ViewProfile extends GenericView
                 echo '<div class="header-profile-container">';
 
                 echo '<div class="profileBanner">
-                    <div style="background: url(\'./Assets/images/banner/' . $bannerFile . '\'); background-size: cover;"></div>
-                </div>';
+                    <div class="banner" style="background: url(\'./Assets/images/banner/' . $bannerFile . '\'); background-size: cover;"></div>
+                ';
 
                 echo '<div class="profileAvatarAndInfos">
                         <div class="profileInfos">
                             <img src="./Assets/images/avatar/'. $avatarFile . '" />
+                            <div class="usernameAndSubscribeButton">
                             <div class="usernameAndInfos">
                                 <h1>'. $username  .'</h1> <span class="memberSince">est membre de ShowBizFlex depuis le '. $registerDate .'.</span>
                             </div>
+                            <div class="subscribe-button">
+                                <button type="submit" id="submit" class="btngradient btngradient-hover color-9">Suivre</button>
+                            </div>
+                            </div>
                         </div>
+                    </div>
                     </div>';
 
                 echo '<div class="profile-body">
                     <div class="panel-info-left">
-                    Salut
+                        <h2 class="panel-subtitle-left"><i class="fa-solid fa-pen-nib"></i> À propos de moi</h2>
+                        <div class="user-about">
+                            <p>'. $about .'</p>
+                        </div>
+
+                        <h2 class="panel-subtitle-left mt-30"><i class="fa-solid fa-star"></i> Genres favoris</h2>
+                        <div class="user-favGenres">
+                            <p>'. $about .'</p>
+                        </div>
+
+                        <h2 class="panel-subtitle-left mt-30"><i class="fa-solid fa-users"></i> Abonnés</h2>
+                        <div class="user-followers">
+                            <p>'. $about .'</p>
+                        </div>
+
+                        <h2 class="panel-subtitle-left mt-30"><i class="fa-solid fa-users"></i> Abonnements</h2>
+                        <div class="user-followers">
+                            <p>'. $about .'</p>
+                        </div>
                     </div>
 
                     <div class="panel-right">
-                    Ca va
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+                        <p>c</p>
+
+                        <p>c</p>
+                        <p>c</p>
                     </div>
                 </div>';
 
